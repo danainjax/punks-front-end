10cdd7ca809fad3a75c8090eb710239de6b9676f
danainjax <danainjax@gmail.com>
2021-09-03 08:36:19 -0400

    hooked front end to backend api and successfully fetched json data from index route, test complete

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 console.log('punks')
+
+const punksDiv = document.getElementById('cryptopunks')
+console.log(punksDiv)
+
+function getPunks() {
+
+    return fetch('http://localhost:3000/punks')
+    .then(response => response.json())
+    .then(data => console.log(data));
+}
+
+getPunks()
