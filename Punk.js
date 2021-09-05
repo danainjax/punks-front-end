bf612c0386fee8b093d7e406751fc70cd93ff34c
danainjax <danainjax@gmail.com>
2021-09-05 13:55:36 -0400

    make a punk object and render to the dom

--- Punk.js
+++ Punk.js
@@ -1,24 +1,34 @@
 class Punk {
     static all = []
-    constructor(punk) {
-        this.punktype = punk.punktype;
-        this.image = punk.image;
-        this.accessories = punk.accessories;
-        this.id = punk.id;
-        Punk.all.push(this);
-    }
+    constructor(data){
+        this.data = data
+        this.constructor.all.push(this)
+      }
+    
+      renderCard = () => {
+        const { punktype, accessories, image,  id } = this.data
+        punksDiv.innerHTML += `
+        <div class="punk-card" data-id=${id}>
+          <img src=${image} alt="punkImage"/>
+          <p class="punktype">${punktype}</p>
+          <p class="punk-number"> Put the punk number or identifier here</p>
+        </div>`
+      }
 
+
+  
      static getPunks() {
 
         return fetch('http://localhost:3000/punks')
         .then(response => response.json())
         .then(punks => {
-            console.log(punks)
-             })
-            }
-  
-
-
+            punks.forEach(punk => new Punk(punk))
+            this.all.forEach(punk => punk.renderCard())
+            
+        })
+        }
+        
+    
 
 
 
