98b197b42f9d5475ab6a829b34cf6b14913c92a0
danainjax <danainjax@gmail.com>
2021-09-25 10:39:53 -0400

    added like fetch post

--- src/components/Like.js
+++ src/components/Like.js
@@ -1,15 +1,25 @@
 class Like {
+  static all = []
   constructor(data) {
     this.data = data;
+    this.constructor.all.push(this)
   }
 
   static addLike = () => {
     const likes = document.querySelectorAll(".likes");
     for (const like of likes) {
       like.addEventListener("click", (e) => {
-        console.log(e.target.dataset.id);
+        console.log(e.target.dataset.id-1);
         if (e.target.innerText == "♡") {
           e.target.innerText = "🖤";
+          const newLike = {
+            punk_id:  e.target.dataset.id,
+            user_id: user.id,
+            count: 1
+
+          }
+          new Like(like)
+          api.createLike(newLike).then(console.log)
         } else {
           e.target.innerText = "♡";
         }
