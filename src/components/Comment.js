c2186215c13f0f0c652895a0b54b893c863c5e63
danainjax <danainjax@gmail.com>
2021-10-03 17:11:35 -0400

    removed console.log

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -57,7 +57,7 @@ class Comment {
   }
 
   render = (comment) => {
-    const { id, text, user_id } = this.data;
+    const { id, text, user_id } = this.data;s
     document.querySelector(".container").innerHTML += `
     <div class="card">
       <p>${text}</p>
