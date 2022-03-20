d4d7fe188aaa2ca610e920cd39dfe55f9a9a1cf7
danainjax <danainjax@gmail.com>
2022-03-20 12:28:54 -0400

    changed api service root var

--- src/index.js
+++ src/index.js
@@ -1,123 +1,121 @@
-const api = new ApiService("http://localhost:3000");
-const main = document.querySelector("#main");
-const login = document.querySelector(".log-in");
-const modal = new Modal();
-const tickerDiv = document.querySelector(".ticker");
+const api = new ApiService('https://punks-api.herokuapp.com')
+const main = document.querySelector('#main')
+const login = document.querySelector('.log-in')
+const modal = new Modal()
+const tickerDiv = document.querySelector('.ticker')
 
 handleDarkTheme = () => {
-  if (document.body.classList == "") {
-    document.body.classList.add("dark-theme");
-    modal.main.classList.add("dark-theme");
-    if (document.body.classList == "dark-theme") {
+  if (document.body.classList == '') {
+    document.body.classList.add('dark-theme')
+    modal.main.classList.add('dark-theme')
+    if (document.body.classList == 'dark-theme') {
       document
-        .querySelectorAll(".punk-card")
-        .forEach((card) => (card.classList = "punk-card-dark-theme"));
+        .querySelectorAll('.punk-card')
+        .forEach((card) => (card.classList = 'punk-card-dark-theme'))
     }
   } else {
-    document.body.classList = "";
-    modal.main.classList = "";
+    document.body.classList = ''
+    modal.main.classList = ''
     document
-      .querySelectorAll(".punk-card-dark-theme")
-      .forEach((card) => (card.classList = "punk-card"));
+      .querySelectorAll('.punk-card-dark-theme')
+      .forEach((card) => (card.classList = 'punk-card'))
   }
-};
-document
-  .querySelector("#dark-theme")
-  .addEventListener("click", handleDarkTheme);
+}
+document.querySelector('#dark-theme').addEventListener('click', handleDarkTheme)
 
 showLoginForm = () => {
-  modal.open();
-  clear();
+  modal.open()
+  clear()
   modal.main.innerHTML = `<form id="login">
   <label for="username">Username</label>
   <input type="text" id="username" name="username">
   <input type="submit" class="log-in" value="Log In">
-</form>`;
+</form>`
   document
-    .querySelector("form")
-    .addEventListener("submit", User.handleUsernameSubmit);
-};
+    .querySelector('form')
+    .addEventListener('submit', User.handleUsernameSubmit)
+}
 
-let user;
+let user
 
-document.querySelector("#login").addEventListener("click", (e) => {
+document.querySelector('#login').addEventListener('click', (e) => {
   if (!localStorage.currentLoggedIn) {
-    showLoginForm();
+    showLoginForm()
   } else {
     alert(
       `${
         JSON.parse(localStorage.currentLoggedIn).username
       } is already logged in.`
-    );
-    if (document.body.classList == "dark-theme") {
+    )
+    if (document.body.classList == 'dark-theme') {
       document
-        .querySelectorAll(".punk-card")
-        .forEach((card) => (card.classList = "punk-card-dark-theme"));
+        .querySelectorAll('.punk-card')
+        .forEach((card) => (card.classList = 'punk-card-dark-theme'))
     }
   }
-});
+})
 
 function clear() {
-  main.innerHTML = "";
-  tickerDiv.innerHTML = "";
+  main.innerHTML = ''
+  tickerDiv.innerHTML = ''
 }
 
 handleLogOutClick = () => {
-  user = "";
-  User.all = [];
-  clear();
-  localStorage.clear();
+  user = ''
+  User.all = []
+  clear()
+  localStorage.clear()
   main.innerHTML = `<h2>PUNKS</h2>
   <img src="assets/images/allPunks.png"/>
-  `;
-};
+  `
+}
 
 handleFindAPunkClick = () => {
-  clear();
-  modal.open();
-  modal.addCloseEventListener();
-  Punk.punkByNumberForm();
-};
+  clear()
+  modal.open()
+  modal.addCloseEventListener()
+  Punk.punkByNumberForm()
+}
 
-document.querySelector("#logout").addEventListener("click", handleLogOutClick);
+document.querySelector('#logout').addEventListener('click', handleLogOutClick)
 
-document.querySelector("#get-punks").addEventListener("click", (e) => {
+document.querySelector('#get-punks').addEventListener('click', (e) => {
   if (!user) {
-    showLoginForm();
+    showLoginForm()
   } else if ((Punk.all = [])) {
-    Punk.getPunks();
+    Punk.getPunks()
   } else {
-    Punk.renderIndex();
+    Punk.renderIndex()
   }
-});
+})
 
-document.querySelector("#find-a-punk").addEventListener("click", (e) => {
-  e.preventDefault();
+document.querySelector('#find-a-punk').addEventListener('click', (e) => {
+  e.preventDefault()
   if (localStorage.currentLoggedIn) {
-    handleFindAPunkClick();
+    handleFindAPunkClick()
   } else {
-    showLoginForm();
+    showLoginForm()
   }
-});
+})
 
-document.querySelector("#my-account").addEventListener("click", (e) => {
-  e.preventDefault();
+document.querySelector('#my-account').addEventListener('click', (e) => {
+  e.preventDefault()
   if (localStorage.currentLoggedIn) {
-    User.greetUser();
+    User.greetUser()
   } else {
-    showLoginForm();
+    showLoginForm()
   }
-});
+})
 
-document.querySelector("#most-likes").addEventListener("click", (e) => {
+document.querySelector('#most-likes').addEventListener('click', (e) => {
   e.preventDefault()
   if (!user) {
-    showLoginForm();
-  } else{
-    api.mostLikes().then(data => {
-      const id = (data.id)
+    showLoginForm()
+  } else {
+    api.mostLikes().then((data) => {
+      const id = data.id
       Punk.find(id).renderShow()
       alert('We love this one!')
-  })
+    })
   }
 })
